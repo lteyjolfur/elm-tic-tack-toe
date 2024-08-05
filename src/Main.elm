0b991129c7080f03a18afe69d6d0309a48039c2c
lteyjolfur@gmail.com <lteyjolfur@gmail.com>
2024-08-06 00:57:37 +0200

    Place X and Os

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,17 @@ module Main exposing (..)
 
 import Browser
 import GameLoop exposing (Msg)
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, h1, h2, text)
 import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html.Events exposing (on, onClick)
+import List exposing (indexedMap)
 
 
 
+-- IDEAS
+-- Custom size
+-- Time travel
+-- Sounds?
 -- MAIN
 
 
@@ -19,6 +24,11 @@ main =
 -- MODEL
 
 
+cellSize : Int
+cellSize =
+    30
+
+
 type CellState
     = Empty
     | X
@@ -26,16 +36,39 @@ type CellState
 
 
 cellToString : CellState -> String
-cellToString cell =
-    case cell of
+cellToString currentCell =
+    case currentCell of
         Empty ->
-            " "
+            "-"
 
         X ->
             "X"
 
         O ->
             "O"
+
+
+toPx : Int -> String
+toPx n =
+    String.fromInt n ++ "px"
+
+
+toggleTurn : CellState -> CellState
+toggleTurn c =
+    if c == X then
+        O
+
+    else
+        X
+
+
+matchCell : CellState -> CellState -> CellState
+matchCell newCell boardCell =
+    if boardCell == Empty then
+        newCell
+
+    else
+        boardCell
 
 
 type alias Model =
@@ -47,7 +80,9 @@ type alias Model =
 
 init : Model
 init =
-    Model X [ List.repeat 9 Empty ] (List.repeat 9 Empty)
+    Model X
+        [ [] ]
+        (List.repeat 9 Empty)
 
 
 
@@ -55,18 +90,30 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Play Int
+    | Toggle Int
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            { model | turn = O }
+        Play index ->
+            let
+                newBoard =
+                    List.indexedMap
+                        (\i c ->
+                            if i == index then
+                                matchCell model.turn c
 
-        Decrement ->
-            { model | turn = X }
+                            else
+                                c
+                        )
+                        model.board
+            in
+            { model | turn = toggleTurn model.turn, board = newBoard }
+
+        Toggle index ->
+            model
 
 
 
@@ -75,7 +122,34 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ style "border" "1px solid #0a0305" ]
-        [ div []
-            (List.map (\cell -> div [] [ text (cellToString cell) ]) model.board)
+    div
+        [ style "box-sizing" "border-box"
+        , style "text-align" "center"
+
+        -- , style "display" "flex"
+        -- , style "justify-items" "center"
+        -- , style "align-items" "center"
         ]
+        [ h1 [] [ text "Tic Tac Toe" ]
+        , h2 [] [ text (cellToString model.turn) ]
+        , div
+            [ style "border" "0.5px solid #0a0305"
+            , style "width" "93px"
+            , style "cursor" "pointer"
+            , style "margin" "0 auto"
+            , style "margin-top" "40px"
+            ]
+            (List.indexedMap (\i c -> cell c i) model.board)
+        ]
+
+
+cell : CellState -> Int -> Html Msg
+cell cState index =
+    div
+        [ style "border" "0.5px solid #0a0305"
+        , style "width" (toPx cellSize)
+        , style "height" (toPx cellSize)
+        , style "display" "inline-block"
+        , onClick (Play index)
+        ]
+        [ text (cellToString cState) ]
